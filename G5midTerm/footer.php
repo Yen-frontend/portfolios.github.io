9661abcaa22832e25ed8fb857653e86b1ee76da5
Yen <seesaw226357@gmail.com>
2024-11-15 16:20:46 +0800

    upload

---
+++ G5midTerm/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-center">
+            <p>2024 &copy; MFEE57G5</p>
+        </div>
+        <div class="float-end">
+        </div>
+    </div>
+</footer>
